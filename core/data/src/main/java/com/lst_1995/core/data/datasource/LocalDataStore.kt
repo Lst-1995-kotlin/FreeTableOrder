2e0336740c92dca31a0f82421b5e81d69a2aa554
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-08-11 22:22:32 +0900

    feat: 자동 로그인을 추후 지원하기 위한 로컬 저장소 설정

--- core/data/src/main/java/com/lst_1995/core/data/datasource/LocalDataStore.kt
+++ core/data/src/main/java/com/lst_1995/core/data/datasource/LocalDataStore.kt
@@ -2,9 +2,22 @@ package com.lst_1995.core.data.datasource
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-class LocalDataStore @Inject constructor(
-    private val dataStore: DataStore<Preferences>
-) {
-}
+class LocalDataStore
+    @Inject
+    constructor(
+        private val dataStore: DataStore<Preferences>,
+    ) {
+        private val authLoginToken = stringPreferencesKey("authLoginToken")
+
+        suspend fun saveLoginToken(token: String) {
+            dataStore.edit { preferences -> preferences[authLoginToken] = token }
+        }
+
+        suspend fun getLoginToken(): String = dataStore.data.map { preferences -> preferences[authLoginToken] ?: "" }.first()
+    }
