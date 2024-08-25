7c19b2a499344f757b34a153e35fb0930de20886
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-08-25 17:30:15 +0900

    chore: 계정 및 모드 설정 후 이동 activity 생성

--- feature/login/src/main/java/com/lst_1995/login/SelectModeFragment.kt
+++ feature/login/src/main/java/com/lst_1995/login/SelectModeFragment.kt
@@ -1,11 +1,14 @@
 package com.lst_1995.login
 
+import android.content.ComponentName
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.addCallback
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
+import com.lst_1995.core.domain.model.ModeType
 import com.lst_1995.core.ui.BaseFragment
 import com.lst_1995.login.databinding.FragmentSelectModeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,15 +29,29 @@ class SelectModeFragment : BaseFragment<FragmentSelectModeBinding>(R.layout.frag
     }
 
     private fun setObserver() {
-        viewModel.loginState.observe(viewLifecycleOwner) { isLogin ->
-            if (isLogin == false) {
-                val navOption =
-                    NavOptions.Builder().setPopUpTo(R.id.selectModeFragment, true).build()
-                findNavController().navigate(
-                    R.id.action_selectModeFragment_to_loginFragment,
-                    null,
-                    navOption,
-                )
+        viewModel.run {
+            loginState.observe(viewLifecycleOwner) { isLogin ->
+                if (isLogin == false) {
+                    val navOption =
+                        NavOptions.Builder().setPopUpTo(R.id.selectModeFragment, true).build()
+                    findNavController().navigate(
+                        R.id.action_selectModeFragment_to_loginFragment,
+                        null,
+                        navOption,
+                    )
+                }
+            }
+            selectMode.observe(viewLifecycleOwner) { mode ->
+                if (mode != ModeType.NONE) {
+                    val intent = Intent()
+                    intent.component =
+                        ComponentName(
+                            "com.lst_1995.freetableorder",
+                            "com.lst_1995.freetableorder.MainActivity",
+                        )
+                    startActivity(intent)
+                    activity?.finish()
+                }
             }
         }
     }
