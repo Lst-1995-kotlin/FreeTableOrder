091a506789f07f55beeb6b1009a629a1bef68c0a
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-08-28 20:51:30 +0900

    remove: 미사용 import문 제거

--- feature/login/src/main/java/com/lst_1995/login/SelectModeViewModel.kt
+++ feature/login/src/main/java/com/lst_1995/login/SelectModeViewModel.kt
@@ -1,7 +1,5 @@
 package com.lst_1995.login
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.lst_1995.core.domain.model.ModeType
