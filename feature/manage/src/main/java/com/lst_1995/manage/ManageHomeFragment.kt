651244a296eb24b66ef6b91ec6443d72addb6a77
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-09-11 14:11:48 +0900

    fix: 관리 홈 화면에서 다른 화면으로 전환시 앱 종료 되던 점 수정

--- feature/manage/src/main/java/com/lst_1995/manage/ManageHomeFragment.kt
+++ feature/manage/src/main/java/com/lst_1995/manage/ManageHomeFragment.kt
@@ -2,6 +2,8 @@ package com.lst_1995.manage
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import com.lst_1995.core.ui.BaseFragment
 import com.lst_1995.manage.databinding.FragmentManageHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,5 +15,38 @@ class ManageHomeFragment : BaseFragment<FragmentManageHomeBinding>(R.layout.frag
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+        setMoveFragment()
+    }
+
+    private fun setMoveFragment() {
+        val navOption =
+            NavOptions
+                .Builder()
+                .setPopUpTo(R.id.manageHomeFragment, true)
+                .build()
+        binding.run {
+            menuManageCv.setOnClickListener {
+                navigateTo(R.id.action_manageHomeFragment_to_manuManageFragment,navOption)
+            }
+            salesHistoryCv.setOnClickListener {
+                navigateTo(R.id.action_manageHomeFragment_to_salesHistoryFragment,navOption)
+            }
+            tableManageCv.setOnClickListener {
+                navigateTo(R.id.action_manageHomeFragment_to_manageTableFragment,navOption)
+            }
+            themeManageCv.setOnClickListener {
+                navigateTo(R.id.action_manageHomeFragment_to_manageThemeFragment,navOption)
+            }
+            passwordManageCv.setOnClickListener {
+                navigateTo(R.id.action_manageHomeFragment_to_managePasswordFragment,navOption)
+            }
+            waitScreenManageCv.setOnClickListener {
+                navigateTo(R.id.action_manageHomeFragment_to_manageWaitScreenFragment,navOption)
+            }
+        }
+    }
+
+    private fun navigateTo(destinationId: Int, navOptions: NavOptions? = null) {
+        findNavController().navigate(destinationId, null, navOptions)
     }
 }
