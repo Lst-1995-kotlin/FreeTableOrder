8fd2b66126bd625783eff4e079c700411eb688c7
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-09-21 19:35:35 +0900

    remove: 기존 미제거 코드 제거

--- core/domain/src/main/java/com/lst_1995/core/domain/usecase/ThemeUseCase.kt
+++ core/domain/src/main/java/com/lst_1995/core/domain/usecase/ThemeUseCase.kt
@@ -1,14 +1,7 @@
 package com.lst_1995.core.domain.usecase
 
-import com.lst_1995.core.domain.repository.LocalDataRepository
 import javax.inject.Inject
 
 class ThemeUseCase
     @Inject
-    constructor(
-        private val localDataRepository: LocalDataRepository,
-    ) {
-        suspend fun saveThemeMode(theme: Int) = localDataRepository.saveThemeMode(theme)
-
-        fun getThemeModeFlow() = localDataRepository.getThemeModeFlow()
-    }
+    constructor()
