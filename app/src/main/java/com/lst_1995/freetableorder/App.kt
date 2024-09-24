8ca44a313d7f6a22024b1d048104974086de3140
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-09-24 14:04:26 +0900

    feat: 앱 시작 시 시스템 설정 값에 따른 테마 적용 기능 작성

--- app/src/main/java/com/lst_1995/freetableorder/App.kt
+++ app/src/main/java/com/lst_1995/freetableorder/App.kt
@@ -1,7 +1,13 @@
 package com.lst_1995.freetableorder
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
