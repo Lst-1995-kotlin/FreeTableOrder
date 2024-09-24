373f7f92015c8a42b7661102b7c34a64cb4571f8
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-09-24 15:43:41 +0900

    refactor: App 클래스 명 수정

--- app/src/main/java/com/lst_1995/freetableorder/FreeTableOrderApp.kt
+++ app/src/main/java/com/lst_1995/freetableorder/FreeTableOrderApp.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application() {
+class FreeTableOrderApp : Application() {
     override fun onCreate() {
         super.onCreate()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
