d672d3cd8a4e75f3feb349c4b252ce22ac4cc3f8
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-09-25 16:49:22 +0900

    refactor: 미사용 매개변수 제거

--- feature/main/src/main/java/com/lst_1995/main/MainViewModel.kt
+++ feature/main/src/main/java/com/lst_1995/main/MainViewModel.kt
@@ -1,34 +1,18 @@
 package com.lst_1995.main
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.lst_1995.core.domain.model.ModeType
-import com.lst_1995.core.domain.usecase.AuthUseCase
 import com.lst_1995.core.domain.usecase.ModeUseCase
 import com.lst_1995.core.domain.usecase.ThemeUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel
     @Inject
     constructor(
-        private val authUseCase: AuthUseCase,
         private val modeUseCase: ModeUseCase,
         private val themeUseCase: ThemeUseCase,
     ) : ViewModel() {
-        val loginState = authUseCase.loginStateFlow()
-        val selectMode = modeUseCase.getPlayModeFlow()
+        val mode = modeUseCase.getPlayModeFlow()
         val theme = themeUseCase.loadAppThemeFlow()
-
-        fun signOut() {
-            viewModelScope.launch {
-                async {
-                    authUseCase.firebaseSignOut()
-                    modeUseCase.savePlayMode(ModeType.NONE)
-                }.await()
-            }
-        }
     }
