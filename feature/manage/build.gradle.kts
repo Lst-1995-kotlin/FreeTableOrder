1cc7ec0b45ff6267c46067a373ef0355c0b71c4c
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-10-02 21:28:55 +0900

    update: coli 라이브러리 추가 및 일부 라이브러리 버전 업데이트

--- feature/manage/build.gradle.kts
+++ feature/manage/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.coil.kt.coil)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
